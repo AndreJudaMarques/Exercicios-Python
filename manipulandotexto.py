ae3d8010fe5da8bad227b932a726e6058fc5b02f
André Judá <judatrainer@gmail.com>
2024-04-04 21:43:07 -0300

    novo

---
+++ manipulandotexto.py
@@ -0,0 +1,20 @@
+# Nessa aula, vamos aprender operações com String no Python. 
+# As principais operações que vamos aprender são o Fatiamento de String, 
+# Análise com len(), count(), find(), transformações com replace(), upper(), lower(), capitalize(), title(), strip(), junção com join().
+
+print()
+
+frase= 'curso em video python'
+
+#fatiamento
+#print(frase[9:13])
+#print(frase[9:21:2])
+#print(frase[:5])
+#print(frase[15:])
+#print(frase[9::3])
+#print(len(frase))
+#print(frase.count('o')) # achou 3 ós
+#print(frase.find('e'))
+print(frase.replace('curso', 'aula'))
+
+print()
