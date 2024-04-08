ed7369381406053bb14e6543d9584eb8718c2a97
André Judá <judatrainer@gmail.com>
2024-04-08 16:04:35 -0300

    novo

---
+++ #28 Escreva um programa que faça o computador “pensar”.py
@@ -0,0 +1,23 @@
+ #28: Escreva um programa que faça o computador “pensar” 
+#em um número inteiro entre 0 e 5 
+#e peça para o usuário tentar descobrir qual foi o número escolhido pelo computador.
+
+from time import sleep
+from random import randrange
+
+print()
+
+print('-=-' * 10)
+print('Vou pensar em um número entre 0 e 5. Tente adivinhar...')
+n = randrange(6) #até o 5
+time.sleep(0.5)
+numero = int(input('Em que número estou pensando? '))
+print('Processando')
+time.sleep(2)
+if numero == n:
+      print(f'PARABÉNS!!! Você acertou meu número!')
+else:
+      print(f'GANHEI!!! Eu pensei no número {n} e não no {numero}!')
+print('-=-' * 10)
+
+print()
