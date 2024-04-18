7ae2f1b819354571b21ace8e2c42d1ed85b63cc4
André Judá <judatrainer@gmail.com>
2024-04-18 14:08:25 -0300

    novo

--- #Exercício Python 059.py
+++ #Exercício Python 059.py
@@ -40,55 +40,55 @@ while op != 5:
             print('   [4] novos números')
             print('   [5] sair do programa')
             op = int(input('>>>> Qual sua opção? '))
-      
-      if op == 2:
-            print(f'A multiplicação entre {numero1} x {numero2} é: {numero1 * numero2:.1f}')
-            print('=-=' * 15)
-            sleep(1)
-            print('   [1] somar')
-            print('   [2] multiplicar')
-            print('   [3] maior')
-            print('   [4] novos números')
-            print('   [5] sair do programa')
-            op = int(input('>>>> Qual sua opção? '))
-      
-      if op == 3:
-            if numero1 > numero2:
-                  print(f'O maior entre {numero1} e {numero2} é: {numero1:.1f}')
-            else:
-                  print(f'O maior entre {numero1} e {numero2} é: {numero2:.1f}')
-            print('=-=' * 15)
-            sleep(1)
-            print('   [1] somar')
-            print('   [2] multiplicar')
-            print('   [3] maior')
-            print('   [4] novos números')
-            print('   [5] sair do programa')
-            op = int(input('>>>> Qual sua opção? '))
-      
-      if op == 4:
-            numero1 = float(input('Digite o primeiro valor: '))
-            numero2 = float(input('Digite o segundo valor: '))
-            print('=-=' * 15)
-            sleep(1)
-            print('   [1] somar')
-            print('   [2] multiplicar')
-            print('   [3] maior')
-            print('   [4] novos números')
-            print('   [5] sair do programa')
-            op = int(input('>>>> Qual sua opção? '))
-      
-      if op > 5:
-            print('Opção inválida. Tente novamente.')
-            print('=-=' * 15)
-            sleep(1)
-            print('   [1] somar')
-            print('   [2] multiplicar')
-            print('   [3] maior')
-            print('   [4] novos números')
-            print('   [5] sair do programa')
-            op = int(input('>>>> Qual sua opção? '))
-            
+                  
+            if op == 2:
+                  print(f'A multiplicação entre {numero1} x {numero2} é: {numero1 * numero2:.1f}')
+                  print('=-=' * 15)
+                  sleep(1)
+                  print('   [1] somar')
+                  print('   [2] multiplicar')
+                  print('   [3] maior')
+                  print('   [4] novos números')
+                  print('   [5] sair do programa')
+                  op = int(input('>>>> Qual sua opção? '))
+                  
+            if op == 3:
+                  if numero1 > numero2:
+                        print(f'O maior entre {numero1} e {numero2} é: {numero1:.1f}')
+                  else:
+                        print(f'O maior entre {numero1} e {numero2} é: {numero2:.1f}')
+                        print('=-=' * 15)
+                        sleep(1)
+                        print('   [1] somar')
+                        print('   [2] multiplicar')
+                        print('   [3] maior')
+                        print('   [4] novos números')
+                        print('   [5] sair do programa')
+                        op = int(input('>>>> Qual sua opção? '))
+                  
+            if op == 4:
+                  numero1 = float(input('Digite o primeiro valor: '))
+                  numero2 = float(input('Digite o segundo valor: '))
+                  print('=-=' * 15)
+                  sleep(1)
+                  print('   [1] somar')
+                  print('   [2] multiplicar')
+                  print('   [3] maior')
+                  print('   [4] novos números')
+                  print('   [5] sair do programa')
+                  op = int(input('>>>> Qual sua opção? '))
+                  
+            if op > 5:
+                  print('Opção inválida. Tente novamente.')
+                  print('=-=' * 15)
+                  sleep(1)
+                  print('   [1] somar')
+                  print('   [2] multiplicar')
+                  print('   [3] maior')
+                  print('   [4] novos números')
+                  print('   [5] sair do programa')
+                  op = int(input('>>>> Qual sua opção? '))
+
 print('Finalizando...')
 sleep(1)
 print('Fim do programa! Volte sempre.')
