b789dab05a7db637bd9c3ab1ae889dbb4e8c87d7
André Judá <judatrainer@gmail.com>
2024-04-24 09:18:08 -0300

    24 4 24

---
+++ Exercício Python 66.py
@@ -0,0 +1,34 @@
+#Exercício Python 66: Crie um programa que leia números inteiros pelo teclado.
+#  O programa só vai parar quando o usuário digitar o valor 999, que é a condição de parada.
+# No final, mostre quantos números foram digitados e qual foi a soma entre elas
+# (desconsiderando o flag).
+
+print()
+
+n = 0
+soma = 0
+cont = 0
+
+while cont > -1:
+      n = int(input('Digite um valor: [999 para parar] : '))
+      if n == 999:
+            break
+      else:
+            cont += 1
+            soma += n
+
+print(f'A soma dos {cont} valores foi {soma}')
+
+print()
+
+
+# n = 0
+# soma = 0
+# cont = 0
+
+# while n != 999:
+#       n = int(input('Digite um valor: '))
+#       cont += 1
+#       soma += n
+
+# print(soma-999)
