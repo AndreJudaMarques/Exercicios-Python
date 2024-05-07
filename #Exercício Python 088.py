774941239796c335f6cfb8b1663759bf54d7c3f9
André Judá <judatrainer@gmail.com>
2024-05-07 14:04:35 -0300

    v

--- #Exercício Python 088.py
+++ #Exercício Python 088.py
@@ -12,7 +12,6 @@ print('---' * 15)
 lista = []
 jogos = []
 
-#sleep(0.5)
 qnt = int(input('Quantos jogos você quer que eu sorteie? '))
 print(f'-=-' * 2, f'{f"SORTEANDO {qnt} JOGOS":^30}', '-=-' * 2)
 
@@ -29,8 +28,10 @@ while cont < qnt:
       lista.sort() 
       jogos.append(lista[:])
       lista.clear()
+sleep(1)
 for indice, listax in enumerate(jogos):
       print(f'Jogo {indice + 1}: {listax}')
+      sleep(0.5)
       
 
 print()
