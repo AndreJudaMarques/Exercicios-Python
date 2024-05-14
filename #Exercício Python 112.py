f7717897c39b9ffc95fadb13d21b3b50b2e866fa
André Judá <judatrainer@gmail.com>
2024-05-14 15:32:10 -0300

    112 bom exemplo

---
+++ #Exercício Python 112.py
@@ -0,0 +1,13 @@
+#Exercício Python 112
+
+print()
+
+
+from utilizadescev import dados
+from utilizadescev import moedas
+
+p = dados.leiadinheiro('Digite o preço: R$')
+moedas.resumo(p, 35, 22)
+
+
+print()
