d6f796624bceb81e5c5d89e7758bb18c5db809a8
André Judá <judatrainer@gmail.com>
2024-05-14 15:36:01 -0300

    14 5 24

--- utilizadescev/dados/__init__.py
+++ utilizadescev/dados/__init__.py
@@ -9,3 +9,15 @@ def leiadinheiro(msg):
             else:
                   valido = True
                   return float(entrada)
+            
+
+def leiaint(msg):
+      valor = 0
+      while True:
+            n = input(msg)
+            if n.isnumeric():
+                  valor = int(n)
+                  break
+            else:
+                  print('\033[0;31mERRO! Digite um número inteiro válido.\033[m')
+      return valor
