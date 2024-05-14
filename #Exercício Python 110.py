180a5b7bd817a8bf82dd0ff4109398f8f2eda684
André Judá <judatrainer@gmail.com>
2024-05-14 11:58:17 -0300

    14 5 24

---
+++ #Exercício Python 110.py
@@ -0,0 +1,6 @@
+#Exercício Python 110
+
+from moedas import resumo
+
+p = float(input('Digite o preço: R$'))
+resumo(p, 30, 12)
--- moedas.py
+++ moedas.py
@@ -7,3 +7,16 @@ def metade(n):
 
 def dobro(n):
       return n * 2
+
+def resumo(n, aum=0, red=0):
+      print('-' * 35)
+      print('          Resumo do Valor    ')
+      print('-' * 35)
+      print(f'Preço analisado:       R${n:.2f}')
+      print(f"Dobro do preço:        R${dobro(n):.2f}")
+      print(f'Metade do preço        R${metade(n):.2f}')
+      print(f'{aum}% de aumento:        R${(n + ((n)*aum) / 100):.2f}')
+      print(f'{red}% de redução:        R${(n - ((n)*red) / 100):.2f}')
+      print('-' * 35)
+
+#resumo(558, 20, 12)
